2f9ca4b54f0ee85fa8ebf1bec0d0a12bb5243313
PickleBoxer <matic.vertacnik@icloud.com>
2023-08-30 14:45:50 +0200

    Autoindex, Php-cs-fixer, Header-stamp

--- pssentry.php
+++ pssentry.php
@@ -1,31 +1,22 @@
 <?php
 /**
-* 2007-2023 PrestaShop
-*
-* NOTICE OF LICENSE
-*
-* This source file is subject to the Academic Free License (AFL 3.0)
-* that is bundled with this package in the file LICENSE.txt.
-* It is also available through the world-wide-web at this URL:
-* http://opensource.org/licenses/afl-3.0.php
-* If you did not receive a copy of the license and are unable to
-* obtain it through the world-wide-web, please send an email
-* to license@prestashop.com so we can send you a copy immediately.
-*
-* DISCLAIMER
-*
-* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
-* versions in the future. If you wish to customize PrestaShop for your
-* needs please refer to http://www.prestashop.com for more information.
-*
-*  @author    PrestaShop SA <contact@prestashop.com>
-*  @copyright 2007-2023 PrestaShop SA
-*  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
-*  International Registered Trademark & Property of PrestaShop SA
-*/
-
-use PSpell\Config;
-
+ * Copyright since 2007 PrestaShop SA and Contributors
+ * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License version 3.0
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/AFL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * @author    PrestaShop SA and Contributors <contact@prestashop.com>
+ * @copyright Since 2007 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License version 3.0
+ */
 if (!defined('_PS_VERSION_')) {
     exit;
 }
@@ -42,7 +33,7 @@ class Pssentry extends Module
         $this->author = 'PickleBoxer';
         $this->need_instance = 0;
 
-        /**
+        /*
          * Set $this->bootstrap to true if your module is compliant with bootstrap (PrestaShop 1.6)
          */
         $this->bootstrap = true;
@@ -52,7 +43,7 @@ class Pssentry extends Module
         $this->displayName = $this->l('Sentry Integrations');
         $this->description = $this->l('Sentry is a developer-first error tracking and performance monitoring platform');
 
-        $this->ps_versions_compliancy = array('min' => '1.7', 'max' => _PS_VERSION_);
+        $this->ps_versions_compliancy = ['min' => '1.7', 'max' => _PS_VERSION_];
     }
 
     /**
@@ -61,11 +52,11 @@ class Pssentry extends Module
      */
     public function install()
     {
-        //Configuration::updateValue('PSSENTRY_DEBUG_MODE', false);
+        // Configuration::updateValue('PSSENTRY_DEBUG_MODE', false);
 
-        return parent::install() &&
-            $this->registerHookAndSetToTop('header') &&
-            $this->registerHook('displayBackOfficeHeader');
+        return parent::install()
+            && $this->registerHookAndSetToTop('header')
+            && $this->registerHook('displayBackOfficeHeader');
     }
 
     public function uninstall()
@@ -94,18 +85,18 @@ class Pssentry extends Module
      */
     public function getContent()
     {
-        /**
+        /*
          * If values have been submitted in the form, process.
          */
-        if (((bool)Tools::isSubmit('submitPssentryModule')) == true) {
+        if (((bool) Tools::isSubmit('submitPssentryModule')) == true) {
             $this->postProcess();
         }
 
         $this->context->smarty->assign('module_dir', $this->_path);
 
-        $output = $this->context->smarty->fetch($this->local_path.'views/templates/admin/configure.tpl');
+        $output = $this->context->smarty->fetch($this->local_path . 'views/templates/admin/configure.tpl');
 
-        return $output.$this->renderForm();
+        return $output . $this->renderForm();
     }
 
     /**
@@ -124,16 +115,16 @@ class Pssentry extends Module
         $helper->identifier = $this->identifier;
         $helper->submit_action = 'submitPssentryModule';
         $helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false)
-            .'&configure='.$this->name.'&tab_module='.$this->tab.'&module_name='.$this->name;
+            . '&configure=' . $this->name . '&tab_module=' . $this->tab . '&module_name=' . $this->name;
         $helper->token = Tools::getAdminTokenLite('AdminModules');
 
-        $helper->tpl_vars = array(
+        $helper->tpl_vars = [
             'fields_value' => $this->getConfigFormValues(), /* Add values for your inputs */
             'languages' => $this->context->controller->getLanguages(),
             'id_language' => $this->context->language->id,
-        );
+        ];
 
-        return $helper->generateForm(array($this->getConfigForm()));
+        return $helper->generateForm([$this->getConfigForm()]);
     }
 
     /**
@@ -141,50 +132,50 @@ class Pssentry extends Module
      */
     protected function getConfigForm()
     {
-        return array(
-            'form' => array(
-                'legend' => array(
+        return [
+            'form' => [
+                'legend' => [
                 'title' => $this->l('Sentry SDK Settings'),
                 'icon' => 'icon-cogs',
-                ),
-                'input' => array(
-                    array(
+                ],
+                'input' => [
+                    [
                         'type' => 'switch',
                         'label' => $this->l('Debug mode'),
                         'name' => 'PSSENTRY_DEBUG_MODE',
                         'is_bool' => true,
                         'desc' => $this->l('Enable or disable Sentry DEBUG mode'),
-                        'values' => array(
-                            array(
+                        'values' => [
+                            [
                                 'id' => 'active_on',
                                 'value' => true,
-                                'label' => $this->l('Enabled')
-                            ),
-                            array(
+                                'label' => $this->l('Enabled'),
+                            ],
+                            [
                                 'id' => 'active_off',
                                 'value' => false,
-                                'label' => $this->l('Disabled')
-                            )
-                        ),
-                    ),
-                    array(
+                                'label' => $this->l('Disabled'),
+                            ],
+                        ],
+                    ],
+                    [
                         'type' => 'text',
                         'label' => $this->l('Loader Script'),
                         'name' => 'PSSENTRY_LOADER_SCRIPT',
                         'desc' => $this->l('You can configure the Loader Script to enable/disable Performance, Replay, and more.'),
-                    ),
-                    array(
+                    ],
+                    [
                         'type' => 'text',
                         'name' => 'PSSENTRY_DSN',
                         'label' => $this->l('DSN'),
                         'desc' => $this->l('The DSN tells the SDK where to send the events to. Show deprecated DSN'),
-                    ),
-                ),
-                'submit' => array(
+                    ],
+                ],
+                'submit' => [
                     'title' => $this->l('Save'),
-                ),
-            ),
-        );
+                ],
+            ],
+        ];
     }
 
     /**
@@ -192,11 +183,11 @@ class Pssentry extends Module
      */
     protected function getConfigFormValues()
     {
-        return array(
+        return [
             'PSSENTRY_DEBUG_MODE' => Configuration::get('PSSENTRY_DEBUG_MODE'),
             'PSSENTRY_LOADER_SCRIPT' => Configuration::get('PSSENTRY_LOADER_SCRIPT'),
             'PSSENTRY_DSN' => Configuration::get('PSSENTRY_DSN'),
-        );
+        ];
     }
 
     /**
@@ -216,16 +207,16 @@ class Pssentry extends Module
     }
 
     /**
-    * Add the CSS & JavaScript files you want to be loaded in the BO.
-    *
-    * public function hookDisplayBackOfficeHeader()
-    * {
-    *     if (Tools::getValue('configure') == $this->name) {
-    *         $this->context->controller->addJS($this->_path.'views/js/back.js');
-    *         $this->context->controller->addCSS($this->_path.'views/css/back.css');
-    *     }
-    * }
-    */
+     * Add the CSS & JavaScript files you want to be loaded in the BO.
+     *
+     * public function hookDisplayBackOfficeHeader()
+     * {
+     *     if (Tools::getValue('configure') == $this->name) {
+     *         $this->context->controller->addJS($this->_path.'views/js/back.js');
+     *         $this->context->controller->addCSS($this->_path.'views/css/back.css');
+     *     }
+     * }
+     */
 
     /**
      * Add the CSS & JavaScript files you want to be added on the FO.
@@ -236,11 +227,11 @@ class Pssentry extends Module
             'sentry_dsn' => Configuration::get('PSSENTRY_DSN'),
             'sentry_loader_script' => Configuration::get('PSSENTRY_LOADER_SCRIPT'),
             'sentry_environment' => _PS_MODE_DEV_ ? 'dev' : 'production',
-            //'sentry_release' => Configuration::get('SENTRY_RELEASE'),
-            'sentry_debug' => Configuration::get('PSSENTRY_DEBUG_MODE')
+            // 'sentry_release' => Configuration::get('SENTRY_RELEASE'),
+            'sentry_debug' => Configuration::get('PSSENTRY_DEBUG_MODE'),
         ]);
 
-        //$this->context->controller->addJS($this->_path.'/views/js/sentry.js');
+        // $this->context->controller->addJS($this->_path.'/views/js/sentry.js');
 
         return $this->display(__FILE__, 'header.tpl');
     }
